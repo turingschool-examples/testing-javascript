051684f9c649921dcb8567f19db1f2f111170b98
Steve Kinney <hello@stevekinney.net>
2015-02-10 10:18:28 -0700

    Add and show same button

--- examples/3-qunit-and-the-dom/show-message.js
+++ examples/3-qunit-and-the-dom/show-message.js
@@ -1,6 +1,11 @@
 var bindClickEventToShowMessageButton = function () {
+  var $message = $('.message');
   $('.show-message').on('click', function () {
-    $('.message').text('This is a secret message.');
+    if ($message.text()) {
+      $message.text('');
+    } else {
+      $message.text('This is a secret message.');
+    }
   });
 };
 
