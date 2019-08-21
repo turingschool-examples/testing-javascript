9a9515cc5de5e84437800fdf25904e1631914e73
Amy Holt <amy.elizabeth.holt@gmail.com>
2019-08-21 10:31:13 -0600

    Remove spaces after function keyword

--- test.js
+++ test.js
@@ -2,8 +2,8 @@
 // var assert = require('chai').assert;
 // var square = require('./implementation');
 
-describe('square', function () {
-  it('returns the square of a number', function () {
+describe('square', function() {
+  it('returns the square of a number', function() {
     assert.equal(square(2), 4);
   });
 });
